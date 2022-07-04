c3b890f32f8599d6d133b276e130df88556a6cc8
Abraham Mitiku <71876193+abriilo@users.noreply.github.com>
2022-07-04 22:38:25 +0300

    blog api http methods added

--- src/api/blog.api.js
+++ src/api/blog.api.js
@@ -9,13 +9,69 @@
 import instance from "./index";
 
 
-export const getBlogs = {
+export const getBlog = {
     /**
      * 
      *  fetching all blogs about businesses
      */
     business: async() =>(
 
-        await instance.get('/business')
+        await instance.get()
     ),
+}
+
+export const updateBlog = {
+    /**
+     * 
+     *  For Updating blogs or any APIS
+     * 
+     
+    [PATCH]
+
+     business: async(id, data) =>(
+
+        await instance.patch(`/${id}`, {data})
+    ),
+
+     / ------------  OR -----------------/
+
+     [POST]
+
+     business: async(id, data) =>(
+
+        await instance.post(`/${id}`, {data})
+    ),
+     */
+}
+
+export const createBlog = {
+    /**
+     * 
+     *  For CREATING API OPERATION
+     * 
+
+
+     [POST]
+
+     business: async(data) =>(
+
+        await instance.post('/your_url_', {data})
+    ),
+     */
+}
+
+export const deleteBlog = {
+    /**
+     * 
+     *  For DELETE OPERATION OF API DATA'S [delete http]
+     * 
+     
+    [PATCH]
+    
+     business: async(id) =>(
+
+        await instance.delete(`/${id}`)
+    ),
+
+     */
 }
